5ca49cd9b0cc25d0be3dd8af738f75dee12d9042
QuocTien <tienlq079@gmail.com>
2022-05-15 22:24:27 +0700

    add pagination function to product-list-items

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
 import {
   CardGroup,
@@ -16,15 +17,25 @@ import {
 
 const truncate = (str , max) => str.length < max ? str : `${str.substr(0, max)}...`;;
 
+const getPaginatedProducts = (products, currentPage, itemsLimit) => {
+  const startIndex = currentPage * itemsLimit - itemsLimit;
+  const endIndex = startIndex + itemsLimit ;
+  return products.slice(startIndex,endIndex)
+};
+
 export default function({products}){
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsLimit, setItemsLimit] = useState(11);
+  const [pagesLimit, setPagesLimit] = useState(5);
 
  
   return (
     <Container>
       <h2> Products </h2>
       <Row>
-        {products.map((product, index) => (
-          <Col md="6" lg="4" key={index}>
+      {getPaginatedProducts(products, currentPage, itemsLimit).map((product, idx) => (
+          <Col md="6" lg="4" key={idx}>
             <Card>
               <CardImg
                 alt="Card image cap"
@@ -53,10 +64,20 @@ export default function({products}){
           </Col>
         ))}
         <Col className="class-col">
-            <div className="page-total">Showing 1 - 16 of 20 total</div>
-            <Button >
+          <div className='show-button'>
+            {itemsLimit < products.length && 
+            <Button onClick={(() => setItemsLimit(itemsLimit + 10))}>
               SHOW MORE
             </Button>
+            }
+            {itemsLimit >= products.length && 
+            <Button variant="secondary" disabled>
+              NO MORE PRODUCT
+            </Button>}
+          </div>
+          <div className='page-total'>
+            Show 1 - {itemsLimit - 1} of {products.length}
+          </div>
         </Col>
       </Row>
       
